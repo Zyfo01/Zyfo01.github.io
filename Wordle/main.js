7686125c0d1f3a0f35ff65a8d458e56568ef445e
Zyfo01 <95011683+Zyfo01@users.noreply.github.com>
2022-03-22 20:57:53 -0400

    Update main.js

--- Wordle/main.js
+++ Wordle/main.js
@@ -1,4 +1,7 @@
-document.addEventListener("keydown",onKeyDown);let currentRow=1;let currentColumn=1;let wordList=[
+document.addEventListener("keydown", onKeyDown);
+let currentRow = 1;
+let currentColumn = 1;
+let dictionary = [
   "CIGAR",
   "REBUT",
   "SISSY",
@@ -12972,30 +12975,370 @@ document.addEventListener("keydown",onKeyDown);let currentRow=1;let currentColum
   "ZYMES",
   "ZYMIC",
 ];
-let wordStr=wordList[Math.round(Math.random()* wordList.length)];let word=[];for(let i=0; i<wordStr.length; i++)word.push(wordStr.charAt(i));let canType=true;
-function onKeyDown(e){if(e.key=="a"||e.key=="b"||e.key=="c"||e.key=="d"||e.key=="e"||e.key=="f"||e.key=="g"||e.key=="h"||e.key=="i"||e.key=="j"||
-e.key=="k"||e.key=="l"||e.key=="m"||e.key=="n"||e.key=="o"||e.key=="p"||e.key=="q"||e.key=="r"||e.key=="s"||e.key=="t"||e.key=="u"||
-e.key=="v"||e.key=="w"||e.key=="x"||e.key=="y"||e.key=="z"){addLetter(e.key.toUpperCase());}else if(e.key=="Backspace"&&currentColumn > 1&&canType){
-currentColumn--;$(getCurrentCellID()).text("");}else if(e.key=="Enter"&&canType){enterWord();}else return;}function getCurrentCellID(){
-let rowLetter;if(currentRow==1)rowLetter="a";if(currentRow==2)rowLetter="b";if(currentRow==3)rowLetter="c";if(currentRow==4)rowLetter="d";
-if(currentRow==5)rowLetter="e";if(currentRow==6)rowLetter="f";return "#"+rowLetter+currentColumn.toString();}function getCellID(column,row){let rowLetter;
-if(row==1)rowLetter="a";if(row==2)rowLetter="b";if(row==3)rowLetter="c";if(row==4)rowLetter="d";if(row==5)rowLetter="e";if(row==6)rowLetter="f";
-return "#"+rowLetter+column.toString();}function rowIsFull(row){let rowLetter;if(row==1)rowLetter="a";if(row==2)rowLetter="b";if(row==3)rowLetter="c";
-if(row==4)rowLetter="d";if(row==5)rowLetter="e";if(row==6)rowLetter="f";if($("#"+rowLetter+"1").text()==""||$("#"+rowLetter+"2").text()==""||
-$("#"+rowLetter+"3").text()==""||$("#"+rowLetter+"4").text()==""||$("#"+rowLetter+"5").text()==""){return false;}else return true;}
-function addLetter(letter){if(currentColumn>=6||!canType)return;$(getCurrentCellID()).text(letter);
-currentColumn++;}function nextLine(){if(currentRow<6){currentRow++;currentColumn=1;}}function enterWord(){if(!rowIsFull(currentRow))return;console.log("enter");
-if(currentRow<6){verifyWord(currentRow);nextLine();}else{verifyWord(currentRow);}}function verifyWord(row){let cellStr =
-$(getCellID(1,row)).text()+$(getCellID(2,row)).text()+$(getCellID(3,row)).text()+$(getCellID(4,row)).text()+$(getCellID(5,row)).text();
-let cellArr=[];for(let i=0; i<5; i++)cellArr.push(cellStr.charAt(i));if(!wordList.includes(cellStr)){
-notInWordList();currentRow--;setTimeout(()=>{currentColumn=6;});return;}if(cellStr==wordStr){$(getCellID(1,row)).css("background","green");$(getCellID(2,row)).css("background","green");
-$(getCellID(3,row)).css("background","green");$(getCellID(4,row)).css("background","green");$(getCellID(5,row)).css("background","green");canType=false;return;}
-for(let i=0;i<cellStr.length;i++){if(word.includes($(getCellID(i+1,row)).text())){$(getCellID(i+1,row)).css("background","rgb(230,200,0)");}
-if(word[i]==$(getCellID(i+1,row)).text()){$(getCellID(i+1,row)).css("background","green");}}}function notInWordList(){$(getCellID(1,currentRow)).css("color","red");
-$(getCellID(2,currentRow)).css("color","red");$(getCellID(3,currentRow)).css("color","red");$(getCellID(4,currentRow)).css("color","red");
-$(getCellID(5,currentRow)).css("color","red");setInterval(()=>{$(getCellID(1,currentRow)).css("color","white");$(getCellID(2,currentRow)).css("color","white");
-$(getCellID(3,currentRow)).css("color","white");$(getCellID(4,currentRow)).css("color","white");$(getCellID(5,currentRow)).css("color","white");},1000);}
+
+let wordList = [
+  "ABUSE",
+  "ADULT",
+  "AGENT",
+  "ANGER",
+  "APPLE",
+  "AWARD",
+  "BASIS",
+  "BEACH",
+  "BIRTH",
+  "BLOCK",
+  "BLOOD",
+  "BOARD",
+  "BRAIN",
+  "BREAD",
+  "BREAK",
+  "BROWN",
+  "BUYER",
+  "CAUSE",
+  "CHAIN",
+  "CHAIR",
+  "CHEST",
+  "CHIEF",
+  "CHILD",
+  "CHINA",
+  "CLAIM",
+  "CLASS",
+  "CLOCK",
+  "COACH",
+  "COAST",
+  "COURT",
+  "COVER",
+  "CREAM",
+  "CRIME",
+  "CROSS",
+  "CROWD",
+  "CROWN",
+  "CYCLE",
+  "DANCE",
+  "DEATH",
+  "DEPTH",
+  "DOUBT",
+  "DRAFT",
+  "DRAMA",
+  "DREAM",
+  "DRESS",
+  "DRINK",
+  "DRIVE",
+  "EARTH",
+  "ENEMY",
+  "ENTRY",
+  "ERROR",
+  "EVENT",
+  "FAITH",
+  "FAULT",
+  "FIELD",
+  "FIGHT",
+  "FINAL",
+  "FLOOR",
+  "FOCUS",
+  "FORCE",
+  "FRAME",
+  "FRANK",
+  "FRONT",
+  "FRUIT",
+  "GLASS",
+  "GRANT",
+  "GRASS",
+  "GREEN",
+  "GROUP",
+  "GUIDE",
+  "HEART",
+  "HENRY",
+  "HORSE",
+  "HOTEL",
+  "HOUSE",
+  "IMAGE",
+  "INDEX",
+  "INPUT",
+  "ISSUE",
+  "JAPAN",
+  "JONES",
+  "JUDGE",
+  "KNIFE",
+  "LAURA",
+  "LAYER",
+  "LEVEL",
+  "LEWIS",
+  "LIGHT",
+  "LIMIT",
+  "LUNCH",
+  "MAJOR",
+  "MARCH",
+  "MATCH",
+  "METAL",
+  "MODEL",
+  "MONEY",
+  "MONTH",
+  "MOTOR",
+  "MOUTH",
+  "MUSIC",
+  "NIGHT",
+  "NOISE",
+  "NORTH",
+  "NOVEL",
+  "NURSE",
+  "OFFER",
+  "ORDER",
+  "OTHER",
+  "OWNER",
+  "PANEL",
+  "PAPER",
+  "PARTY",
+  "PEACE",
+  "PETER",
+  "PHASE",
+  "PHONE",
+  "PIECE",
+  "PILOT",
+  "PITCH",
+  "PLACE",
+  "PLANE",
+  "PLANT",
+  "PLATE",
+  "POINT",
+  "POUND",
+  "POWER",
+  "PRESS",
+  "PRICE",
+  "PRIDE",
+  "PRIZE",
+  "PROOF",
+  "QUEEN",
+  "RADIO",
+  "RANGE",
+  "RATIO",
+  "REPLY",
+  "RIGHT",
+  "RIVER",
+  "ROUND",
+  "ROUTE",
+  "RUGBY",
+  "SCALE",
+  "SCENE",
+  "SCOPE",
+  "SCORE",
+  "SENSE",
+  "SHAPE",
+  "SHARE",
+  "SHEEP",
+  "SHEET",
+  "SHIFT",
+  "SHIRT",
+  "SHOCK",
+  "SIGHT",
+  "SIMON",
+  "SKILL",
+  "SLEEP",
+  "SMILE",
+  "SMITH",
+  "SMOKE",
+  "SOUND",
+  "SOUTH",
+  "SPACE",
+  "SPEED",
+  "SPITE",
+  "SPORT",
+  "SQUAD",
+  "STAFF",
+  "STAGE",
+  "START",
+  "STATE",
+  "STEAM",
+  "STEEL",
+  "STOCK",
+  "STONE",
+  "STORE",
+  "STUDY",
+  "STUFF",
+  "STYLE",
+  "SUGAR",
+  "TABLE",
+  "TASTE",
+  "TERRY",
+  "THEME",
+  "THING",
+  "TITLE",
+  "TOTAL",
+  "TOUCH",
+  "TOWER",
+  "TRACK",
+  "TRADE",
+  "TRAIN",
+  "TREND",
+  "TRIAL",
+  "TRUST",
+  "TRUTH",
+  "UNCLE",
+  "UNION",
+  "UNITY",
+  "VALUE",
+  "VIDEO",
+  "VISIT",
+  "VOICE",
+  "WASTE",
+  "WATCH",
+  "WATER",
+  "WHILE",
+  "WHITE",
+  "WHOLE",
+  "WOMAN",
+  "WORLD",
+  "YOUTH",
+];
+
+let wordStr = wordList[Math.round(Math.random() * wordList.length)];
+let word = [];
+for (let i = 0; i < wordStr.length; i++) word.push(wordStr.charAt(i));
+let canType = true;
+let wordRevealed = false;
+
+function onKeyDown(e) {
+  if (
+    e.key == "a" ||
+    e.key == "b" ||
+    e.key == "c" ||
+    e.key == "d" ||
+    e.key == "e" ||
+    e.key == "f" ||
+    e.key == "g" ||
+    e.key == "h" ||
+    e.key == "i" ||
+    e.key == "j" ||
+    e.key == "k" ||
+    e.key == "l" ||
+    e.key == "m" ||
+    e.key == "n" ||
+    e.key == "o" ||
+    e.key == "p" ||
+    e.key == "q" ||
+    e.key == "r" ||
+    e.key == "s" ||
+    e.key == "t" ||
+    e.key == "u" ||
+    e.key == "v" ||
+    e.key == "w" ||
+    e.key == "x" ||
+    e.key == "y" ||
+    e.key == "z"
+  ) {
+    addLetter(e.key.toUpperCase());
+  } else if (e.key == "Backspace" && currentColumn > 1 && canType) {
+    currentColumn--;
+    $(getCurrentCellID()).text("");
+  } else if (e.key == "Enter" && canType) {
+    enterWord();
+  } else return;
+}
+function getCurrentCellID() {
+  let rowLetter;
+  if (currentRow == 1) rowLetter = "a";
+  if (currentRow == 2) rowLetter = "b";
+  if (currentRow == 3) rowLetter = "c";
+  if (currentRow == 4) rowLetter = "d";
+  if (currentRow == 5) rowLetter = "e";
+  if (currentRow == 6) rowLetter = "f";
+  return "#" + rowLetter + currentColumn.toString();
+}
+function getCellID(column, row) {
+  let rowLetter;
+  if (row == 1) rowLetter = "a";
+  if (row == 2) rowLetter = "b";
+  if (row == 3) rowLetter = "c";
+  if (row == 4) rowLetter = "d";
+  if (row == 5) rowLetter = "e";
+  if (row == 6) rowLetter = "f";
+  return "#" + rowLetter + column.toString();
+}
+function rowIsFull(row) {
+  let rowLetter;
+  if (row == 1) rowLetter = "a";
+  if (row == 2) rowLetter = "b";
+  if (row == 3) rowLetter = "c";
+  if (row == 4) rowLetter = "d";
+  if (row == 5) rowLetter = "e";
+  if (row == 6) rowLetter = "f";
+  if (
+    $("#" + rowLetter + "1").text() == "" ||
+    $("#" + rowLetter + "2").text() == "" ||
+    $("#" + rowLetter + "3").text() == "" ||
+    $("#" + rowLetter + "4").text() == "" ||
+    $("#" + rowLetter + "5").text() == ""
+  ) {
+    return false;
+  } else return true;
+}
+function addLetter(letter) {
+  if (currentColumn >= 6 || !canType) return;
+  $(getCurrentCellID()).text(letter);
+  currentColumn++;
+}
+function nextLine() {
+  currentRow++;
+  currentColumn = 1;
+}
+function enterWord() {
+  if (!rowIsFull(currentRow)) return;
+  verifyWord(currentRow);
+  nextLine();
+}
+function verifyWord(row) {
+  let cellStr =
+    $(getCellID(1, row)).text() +
+    $(getCellID(2, row)).text() +
+    $(getCellID(3, row)).text() +
+    $(getCellID(4, row)).text() +
+    $(getCellID(5, row)).text();
+  let cellArr = [];
+  for (let i = 0; i < 5; i++) cellArr.push(cellStr.charAt(i));
+  if (!dictionary.includes(cellStr)) {
+    notInWordList();
+    currentRow--;
+    setTimeout(() => {
+      currentColumn = 6;
+    });
+    return;
+  }
+  if (cellStr == wordStr) {
+    $(getCellID(1, row)).css("background", "green");
+    $(getCellID(2, row)).css("background", "green");
+    $(getCellID(3, row)).css("background", "green");
+    $(getCellID(4, row)).css("background", "green");
+    $(getCellID(5, row)).css("background", "green");
+    canType = false;
+    return;
+  }
+  for (let i = 0; i < cellStr.length; i++) {
+    if (word.includes($(getCellID(i + 1, row)).text())) {
+      $(getCellID(i + 1, row)).css("background", "rgb(230,200,0)");
+    }
+    if (word[i] == $(getCellID(i + 1, row)).text()) {
+      $(getCellID(i + 1, row)).css("background", "green");
+    }
+  }
+}
+function notInWordList() {
+  $(getCellID(1, currentRow)).css("color", "red");
+  $(getCellID(2, currentRow)).css("color", "red");
+  $(getCellID(3, currentRow)).css("color", "red");
+  $(getCellID(4, currentRow)).css("color", "red");
+  $(getCellID(5, currentRow)).css("color", "red");
+  setInterval(() => {
+    $(getCellID(1, currentRow)).css("color", "white");
+    $(getCellID(2, currentRow)).css("color", "white");
+    $(getCellID(3, currentRow)).css("color", "white");
+    $(getCellID(4, currentRow)).css("color", "white");
+    $(getCellID(5, currentRow)).css("color", "white");
+  }, 1000);
+}
+
 setInterval(() => {
-  if (currentRow > 6) alert(word);
-  row = 6;
+  if (currentRow > 6 && !wordRevealed) {
+    wordRevealed = true;
+    alert("The word was: " + wordStr);
+  }
 });
